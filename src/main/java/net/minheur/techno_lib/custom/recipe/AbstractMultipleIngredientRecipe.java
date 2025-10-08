0324be1a4e2f38b09d8dead80e9e0f78592900b4
Minheur2000 <adrien.rousselet@hotmail.com>
2025-10-08 16:26:10 +0200

    patched recipe

--- src/main/java/net/minheur/techno_lib/custom/recipe/AbstractMultipleIngredientRecipe.java
+++ src/main/java/net/minheur/techno_lib/custom/recipe/AbstractMultipleIngredientRecipe.java
@@ -2,13 +2,14 @@ package net.minheur.techno_lib.custom.recipe;
 
 import net.minecraft.core.NonNullList;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
 
-public abstract class AbstractIngredientRecipe extends AbstractRecipe {
+public abstract class AbstractMultipleIngredientRecipe extends AbstractResultRecipe {
     protected final NonNullList<Ingredient> ingredients;
 
-    protected AbstractIngredientRecipe(ResourceLocation id, NonNullList<Ingredient> ingredients) {
-        super(id);
+    protected AbstractMultipleIngredientRecipe(ResourceLocation id, NonNullList<Ingredient> ingredients, ItemStack output) {
+        super(id, output);
         this.ingredients = ingredients;
     }
 
