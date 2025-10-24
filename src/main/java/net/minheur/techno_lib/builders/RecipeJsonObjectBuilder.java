5e99409210930c9cc7e502ea94bf932311cef445
Minheur2000 <adrien.rousselet@hotmail.com>
2025-10-24 11:42:18 +0200

    added a builder for json recipe ingredient & result

---
+++ src/main/java/net/minheur/techno_lib/builders/RecipeJsonObjectBuilder.java
@@ -0,0 +1,43 @@
+package net.minheur.techno_lib.builders;
+
+import com.google.gson.JsonObject;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.ItemLike;
+
+import static net.minheur.techno_lib.Utils.getBuiltInItemRegistry;
+
+public class RecipeJsonObjectBuilder {
+    private final JsonObject object;
+
+    public RecipeJsonObjectBuilder() {
+        this.object = new JsonObject();
+    }
+
+    public RecipeJsonObjectBuilder addItem(ItemLike item) {
+        object.addProperty("item", getBuiltInItemRegistry(item));
+        return this;
+    }
+    public RecipeJsonObjectBuilder addFluid(String fluid, int amount) {
+        object.addProperty("fluid", fluid);
+        object.addProperty("amount", amount);
+        return this;
+    }
+    public RecipeJsonObjectBuilder addTag(TagKey<Item> tag) {
+        object.addProperty("tag", tag.location().toString());
+        return this;
+    }
+
+    public RecipeJsonObjectBuilder addCount(int count) {
+        if (count > 1) object.addProperty("count", count);
+        return this;
+    }
+    public RecipeJsonObjectBuilder addChance(float chance) {
+        if ((chance > 0) && !(chance >= 1)) object.addProperty("chance", chance);
+        return this;
+    }
+
+    public JsonObject build() {
+        return object;
+    }
+}
