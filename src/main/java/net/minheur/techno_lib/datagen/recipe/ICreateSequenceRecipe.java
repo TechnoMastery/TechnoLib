7662ff9949eafd05faaa768956300652f384260a
Minheur4000 <ordi2526883@educ.normandie.fr>
2025-10-31 15:45:42 +0100

    updated create sequence recipe (re-re)

--- src/main/java/net/minheur/techno_lib/datagen/recipe/ICreateSequenceRecipe.java
+++ src/main/java/net/minheur/techno_lib/datagen/recipe/ICreateSequenceRecipe.java
@@ -2,8 +2,6 @@ package net.minheur.techno_lib.datagen.recipe;
 
 import com.google.gson.JsonObject;
 import net.minecraft.data.recipes.FinishedRecipe;
-import net.minecraft.world.level.ItemLike;
-import net.minheur.techno_lib.builders.JsonBuilder;
 
 public interface ICreateSequenceRecipe {
       FinishedRecipe getFinishedRecipe();
